ae701f19c0e7e2b69e6f48db25cc5270698d4b8b
nmset0 <nmset0@colostate.edu>
2024-11-12 17:24:01 -0700

    Model ideas

---
+++ nathan_workspace.R
@@ -0,0 +1,35 @@
+library(readr)
+library(recipes)
+library(yardstick)
+library(parsnip)
+library(purrr)
+library(discrim)
+library(rsample)
+library(workflows)
+library(tune)
+library(dplyr)
+library(tidyverse)
+library(rpart)
+library(leaps)
+library(glmnet)
+library(pls)
+library(workflowsets)
+
+
+test = read_csv("test.csv")
+train = read_csv("train.csv")
+
+
+# Logistic Regression
+
+# K-Nearest Neighbors
+
+# Regression Spline
+
+# Random Forest
+
+# Support Vector Machine
+
+# Linear Discriminant Analysis / Quadratic Discriminant Analysis ??
+
+# Maybe include model validation?
