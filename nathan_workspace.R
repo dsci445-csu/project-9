4c69639a8351e44d679fafa7937292e1fc26b5be
nmset0 <nmset0@colostate.edu>
2024-11-25 17:30:04 -0700

    Attempting LDA

--- nathan_workspace.R
+++ nathan_workspace.R
@@ -15,25 +15,44 @@ library(glmnet)
 library(pls)
 library(workflowsets)
 library(nnet) # multinom logistic reg
+library(mice)
+library(naniar)
+library(mitools)
+library(e1071)
+
+# K-Nearest Neighbors (Eddie)
+# Random Forest (Kwan)
+# Neural Networks?
 
 
 test = read_csv("test.csv")
 train = read_csv("train.csv")
 # data_dictionary = read_csv("data_dictionary.csv")
 
+# Cleaning
+train_clean = train %>% select(-ends_with("Season")) 
+train_clean$sii = factor(train_clean$sii)
+
+colnames(train_clean) = gsub("-", "_", colnames(train_clean))
+
+# Replacing NA with "Missing" level
+# train_clean$sii <- addNA(train_clean$sii)
+#levels(train_clean$sii)[is.na(levels(train_clean$sii))] <- "Missing"
+
+# Replacing NA with the mode level
+# mode_value <- names(which.max(table(train_clean$sii)))
+# train_clean$sii[is.na(train_clean$sii)] <- mode_value
+
+# end
 
 
-# Multinomial Logistic Regression
 
-# K-Nearest Neighbors
+# Linear Discriminant Analysis: 
 
-# Regression Spline
 
-# Random Forest
 
-# Support Vector Machine
+# Support Vector Machines
 
-# Linear Discriminant Analysis / Quadratic Discriminant Analysis ??
 
-# Include model validation?
+
 
