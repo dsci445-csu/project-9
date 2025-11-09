589e741ab4786d311607fcfd95260197bd377c59
elleangell826 <eangell@colostate.edu>
2025-11-09 16:03:37 -0700

    grabbed some event stats

--- data-collection/elle-test.py
+++ data-collection/elle-test.py
@@ -10,8 +10,23 @@ async def main():
   match_ids = await match_ids_url(None, players, count)
   timeline = await timeline_url(None, match_ids)
   frames = timeline['info']['frames']
-  # filter games less than 15 minutes == 900 seconds
   # info -> frames -> events -> timestamp
-  if frames[-1]['timestamp'] < 15*60000:
+  if frames[-1]['timestamp'] < 15*60000: # filter games less than 15 minutes
     print("Skip matches less than 15 minutes")
-    continue;
+  else: # valid match
+    # track meaningful events
+    total_kills = 0
+    elite_monster = 0
+    turrets_destroyed = 0
+    for frame in frames:
+      for event in frame['events']:
+        type = event['type']
+        # add meaningful events
+        if type == 'CHAMPION_KILL':
+          total_kills+=1
+        if type == 'ELITE_MONSTER_KILL':
+          elite_monster+=1
+        if type == 'BUILDING_KILL':
+          turrets_destroyed+=1
+          
+      
