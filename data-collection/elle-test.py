486ad3a23990fbedaf61aeaa11fa2125918f97e3
elleangell826 <eangell@colostate.edu>
2025-11-09 15:18:58 -0700

    added [-1] to look at last frame for filtering

--- data-collection/elle-test.py
+++ data-collection/elle-test.py
@@ -2,14 +2,16 @@ import asyncio
 from requesturls import match_ids_url, timeline_url
 
 puuid = "your puuid here"
-count = 10
+count = 10 # num of most recent games
 
 async def main():
   players = [{'puuid' : puuid}]
-  match_ids = match_ids_url(None, players, count)
-  timeline = timeline_url(None, match_ids)
-  frames = timeline['InfoTimeLineDto']['frames']
+  # only grabs urls not data yet
+  match_ids = await match_ids_url(None, players, count)
+  timeline = await timeline_url(None, match_ids)
+  frames = timeline['info']['frames']
   # filter games less than 15 minutes == 900 seconds
-  if frames['timestamp'] < 15*60000:
+  # info -> frames -> events -> timestamp
+  if frames[-1]['timestamp'] < 15*60000:
     print("Skip matches less than 15 minutes")
     continue;
