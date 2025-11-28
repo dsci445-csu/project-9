7f153a745f63229bb8743cbdec86d05e67049f59
elleangell826 <eangell@colostate.edu>
2025-11-27 21:13:57 -0700

    created separate functions for each task

--- data-collection/elle-test.py
+++ data-collection/elle-test.py
@@ -4,29 +4,26 @@ import json
 from requesturls import match_ids_url, timeline_url
 from helpers import write_json_list
 
-puuid = "puuid here"
-api_key = "api key here"
-count = 1 # num of most recent games
-
-async def test():
+async def get_match_data(puuid, api_key, count):
     match_ids = await match_ids_url(puuid, api_key, count)
     async with aiohttp.ClientSession() as session:
      async with session.get(match_ids) as resp:
         match_ids = await resp.json()
-    timel = await timeline_url(match_ids[0], api_key)
+         return match_ids
+
+async def get_timeline_data(match_id, api_key):
+    timeline_url = await timeline_url(match_ids[0], api_key)
     async with aiohttp.ClientSession() as session:
-     async with session.get(timel) as resp:
+     async with session.get(timeline_url) as resp:
         timeline = await resp.json()
-    frames = timeline['info']['frames']
-    # info -> frames -> events -> timestamp
-    if frames[-1]['timestamp'] < 15*60000: # filter games less than 15 minutes
-      print("Skip matches less than 15 minutes")
-    else: # valid match
-      # track meaningful events
-      total_kills = 0
-      elite_monster = 0
-      turrets_destroyed = 0
-      for frame in frames:
+         return timeline
+
+async def track_events(frames):
+    # track meaningful events
+    total_kills = 0
+    elite_monster = 0
+    turrets_destroyed = 0
+    for frame in frames:
         for event in frame['events']:
           t = event['type']
           # add meaningful events
@@ -36,10 +33,21 @@ async def test():
             elite_monster+=1
           if t == 'BUILDING_KILL':
             turrets_destroyed+=1
-          # minions killed, player gold over time
+          # add more events
+    return total_kills, elite_monster, turrets_destroyed
+    
+async def test():
+    match_data = await get_match_data(puuid, api_key, count)
+    timeline_data = await get_timeline_data(match_id, api_key)
+    
+    frames = timeline_data['info']['frames']
+    # info -> frames -> events -> timestamp
+    if frames[-1]['timestamp'] < 15*60000: # filter games less than 15 minutes
+      print("Skip matches less than 15 minutes")
+    else:
       print(f"Total kills: {total_kills}")
       print(f"Total elite monsters killed: {elite_monster}")
       print(f"Total turrets destroyed: {turrets_destroyed}")
-final=asyncio.run(test())
+final = .run(test())
 print(json.dumps(final,indent=2))          
       
